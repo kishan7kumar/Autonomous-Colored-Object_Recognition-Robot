f404c6b8800dff63987046fc0ddf228a697ad7e2
Kishan Kumar <53033119+kishan7kumar@users.noreply.github.com>
2022-01-12 22:51:09 +0530

    Update Final-Test-Run.ino

--- Final-Test-Run.ino
+++ Final-Test-Run.ino
@@ -1,6 +1,3 @@
-
-// program for AUTONOMOUS COLORED OBJECT RECOGNIZING BOT**************************
-
 #include <Servo.h> // for servo motor
 Servo myservo;     // for servo motor
 int pos = 0;       //  position for servo motor
@@ -15,11 +12,11 @@ int frequency1 = 0; // for red diode
 int frequency2 = 0; // for green diode
 int frequency3 = 0; // for blue diode
 
-// defining my motor 1
+// defining dc motor 1
 const int in1 = 2;
 const int in2 = 3;
 
-// defining my motor 2
+// defining dc motor 2
 const int in3 = 10;
 const int in4 = 11;
 
@@ -31,12 +28,9 @@ long duration;
 int distance;
 
 void setup()
-
 {
-
     //for servo motor
     myservo.attach(9);
-
     // pins for color sensor
     pinMode(S0, OUTPUT);
     pinMode(S1, OUTPUT);
@@ -45,8 +39,8 @@ void setup()
     pinMode(sensorOut, INPUT);
     digitalWrite(S0, HIGH); // Setting frequency-scaling to 20% of color sensor
     digitalWrite(S1, LOW);
-    //Serial.begin(9600);              // for displaying results on laptop screen
-
+    //Serial.begin(9600);   // for displaying results on laptop screen
+    
     // for motor control
     pinMode(in1, OUTPUT); // motor 1
     pinMode(in2, OUTPUT);
@@ -56,56 +50,45 @@ void setup()
     // for ultrasonic sensor
     pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
     pinMode(echoPin, INPUT);  // Sets the echoPin as an Input
-                              //Serial.begin(9600);            // for displaying in the monitor
+    //Serial.begin(9600);            // for displaying in the monitor
 }
 
+//ROBOT STARTS WORKING USING ARDUINO LOOP FUNCTION
 void loop()
-
 {
-    detectdistance();
-    left(); //keep searching 360 degrees
-
-    // if object found
-    if (distance <= 40) // object found within range of 40 cm
+    detectdistance(); // start detecting objects nearby
+    left(); //robot rotates continuously on its place and keep searching for object 
+    if (distance <= 40) // ultrasonic sensor detects any object within range of 40 cm
     {
-        forward(); // move towards object
-        detectdistance();
+        forward(); // robot moves towards the object
+        detectdistance(); //keep detecting distance
         if (distance > 3 && distance <= 13) // if object is 13cm away
         {
             halt(); // bot stops
             delay(1000);
-            //left();                 // to correct its gripper position wrt to object
-            //delay(100);
-            // halt();
-            //delay(500);
-
-            detectcolor(); // calling detectcolor function to detect color using tcs 3200
-            delay(5000);   // wait for 2 second to detect color
-
+            detectcolor(); // starts to detect color using tcs 3200
+            delay(5000);   // wait for 5 second to detect color
             if (frequency1 > 30 && frequency1 < 270) // settting condition for checking if object has RED color
             {
-
                 myservo.write(-80); // open up the gripper
                 delay(2000);
                 forward();
                 delay(190);
                 halt();
-                delay(2000);       // wait for 2 second
+                delay(2000);    
                 myservo.write(80); // close the gripper
-                delay(2000);       //wait for 2 second
-
-                //  as red color is detected now bot goes backward to original location
+                delay(2000);     
+                //  As red colored object is detected now bot goes backward to original location
                 // bot reverses
                 backward();
                 delay(2000); // bot goes back for 3 second
                 left();      // take left to place the object
                 delay(2000); // take left for 2 seconds
-                             // now bot stops
                 halt();      // bot stops for 1 second
                 delay(1000);
 
                 // bot keeps the object in ground
-                forward(); // move little forward
+                forward(); 
                 delay(400);
                 halt();
                 delay(1000);
@@ -117,49 +100,36 @@ void loop()
                 delay(1000);
                 myservo.write(80); // grips closed
                 delay(2000);       // wait for 2 second
-
                 right();     // come in centre again by taking right
                 delay(4000); // take right for 4 seconds
                 halt();
                 delay(5000); // wait for 5 second to start process again
 
-            } // if condtion closed of colour process
-
+            } 
             else // if red color was not detected then go back to original location
-
             {
                 backward(); // go back to original location
                 delay(2000);
-
                 right(); // searching position
                 delay(2000);
-
-                // turn motors off
-                halt();
+                halt(); // turn motors off
                 delay(5000);
+            } 
+        } 
+    }    
+}
 
-            } //else condition closed of colour process
-
-        } //   if closed of near distance
-    }     // if closed of search process
-
-} // void loop function closed
-
-// ALL USED FUNCTIONS DEFINED HERE
-
+// ****************************. ALL FUNCTIONS DEFINED HERE
 void detectcolor()
 {
-
     //Setting red filtered photodiodes to be read
     digitalWrite(S2, LOW);
     digitalWrite(S3, LOW);
     frequency1 = pulseIn(sensorOut, LOW); // Reading the output frequency of red color
-
     //Setting Green filtered photodiodes to be read
     digitalWrite(S2, HIGH);
     digitalWrite(S3, HIGH);
     frequency2 = pulseIn(sensorOut, LOW); // Reading the output frequency of green color
-
     //Setting Blue filtered photodiodes to be read
     digitalWrite(S2, LOW);
     digitalWrite(S3, HIGH);
@@ -168,7 +138,6 @@ void detectcolor()
 
 void detectdistance()
 {
-
     digitalWrite(trigPin, LOW); // Clears the trigPin
     delayMicroseconds(2);
     digitalWrite(trigPin, HIGH); // Sets the trigPin on HIGH state for 10 micro seconds
@@ -176,8 +145,7 @@ void detectdistance()
     digitalWrite(trigPin, LOW);
     duration = pulseIn(echoPin, HIGH); // Reads the echoPin, returns the sound wave travel time in microseconds
     distance = duration * 0.034 / 2;   // Calculating the distance
-                                       //Serial.print("Distance: ");         // Prints the distance on the Serial Monitor
-                                       // Serial.println(distance);
+    //Serial.print("Distance: ");         // Prints the distance on the Serial Monitor                          
 }
 
 void forward()
@@ -211,6 +179,7 @@ void left()
     digitalWrite(in3, LOW); // turn on motor 2 reverse direction
     digitalWrite(in4, HIGH);
 }
+
 void right()
 {
     digitalWrite(in1, LOW); // turn on motor 1 reverse direction
